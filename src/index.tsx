2ec5b00a8d05afbbe6f580bd65a604aef502e2e8
dowens89 <d.l.owens@icloud.com>
2026-06-16 11:17:15 +0100

    Remove password gate

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,5 @@ import "./index.css";
 import React from "react";
 import { render } from "react-dom";
 import { App } from "./App";
-import { PasswordGate } from "./components/PasswordGate";
 
-render(
-  <PasswordGate>
-    <App />
-  </PasswordGate>,
-  document.getElementById("root")
-);
+render(<App />, document.getElementById("root"));
